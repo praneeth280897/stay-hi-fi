410e214685c456485afe50abd69007008375c5e0
“Praneeth <reddypraneeth94@gmail.com>
2026-02-02 10:43:56 +0530

    changes for ping

--- src/main/java/com/example/stay_hi_fi/controller/HealthCheckerController.java
+++ src/main/java/com/example/stay_hi_fi/controller/HealthCheckerController.java
@@ -1,13 +1,16 @@
 package com.example.stay_hi_fi.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class HealthCheckerController {
 
     @GetMapping("/api/public/ping")
     public String ping() {
+        log.info("Ping Successful");
         return "Server is awake!";
     }
 }
